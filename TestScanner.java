3ba1891b93c5592bc1fe9a8441b35b02b3b9159d
kang937035773 <937035773@qq.com>
2017-08-08 09:36:53 +0800

    JAVA中的时间类

---
+++ TestScanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TestScanner {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		
+		System.out.println(str);
+	}
+}
